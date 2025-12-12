1d155738670999aa5229a103c6f1f9b0b972f234
juanCoder <jgama12569@gmail.com>
2025-12-12 17:17:19 -0600

    last commit had some errors

--- src/getting_started.ts
+++ src/getting_started.ts
@@ -7,3 +7,20 @@ const localTs = window.ts;
 const codeInput = document.getElementById("codeInput") as HTMLTextAreaElement | null;
 const runBtn = document.getElementById("runBtn") as HTMLButtonElement | null;
 const codeOutput = document.getElementById("codeOutput") as HTMLElement | null;
+function run(): void {
+    const code = codeInput?.value || "";
+    if(codeOutput) {
+        codeOutput.textContent = localTs.transpileModule(code, {
+            compilerOptions: { module: localTs.ModuleKind.ESNext, target: localTs.ScriptTarget.ES2015 },
+        }).outputText;
+    }
+}
+
+
+runBtn?.addEventListener("click", run);
+
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", () => run());
+} else {
+    run();
+}
